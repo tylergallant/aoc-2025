0e75b1ef485e8a646503841903de1f16cea6940f
Tyler Gallant <tylergallant@live.com>
2025-12-13 13:38:00 -0500

    day 2 part 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,17 +1,31 @@
-module Day02 where
+module Day02 (day02) where
 
 import Data.Char (isDigit)
+import Data.List (inits, stripPrefix)
 import Text.ParserCombinators.ReadP (ReadP, char, munch1, sepBy)
 import Utils (createSolver, getInput)
 
 type Id = String
+type IdSegment = String
 type Range = (Id, Id)
 
-idInvalid :: Id -> Bool
-idInvalid i = a == b
+solution1 :: Id -> Bool
+solution1 i = a == b
   where
     (a, b) = splitHalf i
     splitHalf xs = let n = length xs `div` 2 in splitAt n xs
+
+checkCandidatePattern :: IdSegment -> Id -> Bool
+checkCandidatePattern _ [] = True
+checkCandidatePattern pattern i = maybe False checkNext $ stripPrefix pattern i
+  where checkNext = checkCandidatePattern pattern
+
+candidatePatterns :: Id -> [IdSegment]
+candidatePatterns i = take upToHalfDigits . drop 1 $ inits i
+  where upToHalfDigits = length i `div` 2
+
+solution2 :: Id -> Bool
+solution2 i = any (flip checkCandidatePattern i) $ candidatePatterns i
 
 expandRange :: Range -> [Id]
 expandRange (a, b) = show <$> ids
@@ -24,11 +38,13 @@ parser = sepBy parseRange sep <* char '\n'
     parseId = munch1 isDigit
     parseRange = (\a _ b -> (a, b)) <$> parseId <*> char '-' <*> parseId
 
-solution1 :: [Range] -> Int
-solution1 ranges = sum . map read . filter idInvalid $ ranges >>= expandRange
+makeSolution :: (Id -> Bool) -> [Range] -> Int
+makeSolution solution ranges = sum . map read $ filter solution ids
+  where ids = ranges >>= expandRange
 
 day02 :: IO ()
 day02 = do
   input <- getInput "day02-input.txt"
   let solve = createSolver parser input
-  solve solution1
+  solve $ makeSolution solution1
+  solve $ makeSolution solution2
